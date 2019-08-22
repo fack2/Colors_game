79342caa6b13939327c6d30445c5d4fb306cbb15
YousefQwasmeh <151037@ppu.edu.ps>
2019-08-22 09:12:31 +0300

    add images

--- src/component/GameOver/index.js
+++ src/component/GameOver/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 
 class GameOver extends Component {
-
   over = event => {
-    if (event.target.name == "repeat") {
+    if (event.target.name === "repeat") {
       this.props.status(0);
     } else this.props.status(-1);
   };
@@ -21,6 +20,10 @@ class GameOver extends Component {
         <button className="gameOver" name="playAgain" onClick={this.over}>
           Play Again
         </button>
+        <img
+          alt="img game over"
+          src="https://media.istockphoto.com/vectors/game-over-skull-vector-icon-vector-id1098194540?k=6&m=1098194540&s=170667a&w=0&h=C_NMg6BRy8pP2_Er43P61Gmk4LWff3w0Uf38WTA0xeA="
+        />
       </div>
     );
   }
